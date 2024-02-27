ac35a793422e8d7be5024d81b52f5e41c62ecb79
NandayGB <fernandagbarbosa97@gmail.com>
2024-02-27 20:54:36 -0300

    segundo commit no novo branch

--- src/ecommerce/Menu.java
+++ src/ecommerce/Menu.java
@@ -1,11 +1,13 @@
 package ecommerce;
 
+
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import ecommerce.model.Panelas;
+import ecommerce.controller.Produtocontroller;
 import ecommerce.model.Frigideiras;
-import ecommerce.model.Produto;
-
 import ecommerce.util.Cores;
 
 
@@ -13,10 +15,8 @@ public class Menu {
 
     public static void main(String[] args) {
 
-    	Produto a1 = new Produto(123, "frigideira de ferro", 1, 100.00f);
-		a1.visualizar(); 
-		
     	
+		Produtocontroller produtos = new Produtocontroller();
     	Scanner leia = new Scanner(System.in);
 
         int opcao;
@@ -48,30 +48,99 @@ public class Menu {
             }
 
             switch (opcao) {
+            
                 case 1:
-                    System.out.println("Criar Produto\n\n");
-                   
+                	System.out.println("Criar Produto\n\n");
+                    System.out.println("Digite o ID do Produto: ");
+                    int id = leia.nextInt();
+                    System.out.println("Digite o Nome do Produto: ");
+                    leia.nextLine(); 
+                    String nome = leia.nextLine();
+                    System.out.println("Digite o Tipo do Produto (1-Panela ou 2-Frigideira): ");
+                    int tipo = leia.nextInt();
+                    System.out.println("Digite o Preço do Produto R$");
+                    float preco = leia.nextFloat();
+
+                    if (tipo == 1) {
+                        produtos.criarProduto(new Panelas(id, nome, tipo, preco, "Aço Inox"));
+                    } else if (tipo == 2) {
+                        produtos.criarProduto(new Frigideiras(id, nome, tipo, preco, "Ferro"));
+                    } else {
+                        System.out.println("Tipo de produto inválido.");
+                    }
+                    keyPress();
                     break;
+                    
                 case 2:
                     System.out.println("Listar Todos os Produtos\n\n");
-                   
+                    produtos.listarProdutos();
+                    keyPress();
                     break;
+                    
+           
                 case 3:
+                	
                     System.out.println("Consultar Produtos - por ID\n\n");
-                   
+                    System.out.println("Digite o ID do Produto: ");
+                    int numero = leia.nextInt();
+                    produtos.consultarProdutoPorId(numero);
+                    keyPress();
                     break;
+                    
                 case 4:
                     System.out.println("Atualizar Produto\n\n");
-                    
+                    System.out.println("Digite o ID do Produto: ");
+                    numero = leia.nextInt();
+
+                    var buscaProduto = produtos.buscarNaCollection(numero);
+
+                    if (buscaProduto != null) {
+                        tipo = buscaProduto.getTipo();
+
+                        System.out.println("Digite o Novo Nome do Produto: ");
+                        leia.nextLine(); // Limpa o buffer do teclado
+                        nome = leia.nextLine();
+                        System.out.println("Digite o Novo Preço do Produto R$");
+                        preco = leia.nextFloat();
+
+                        if (tipo == 1) {
+                            produtos.atualizarProduto(new Panelas(numero, nome, tipo, preco, "Aço Inox"));
+                        } else if (tipo == 2) {
+                            produtos.atualizarProduto(new Frigideiras(numero, nome, tipo, preco, "Ferro"));
+                        }
+                    } else {
+                        System.out.println("\nO Produto com ID " + numero + " não foi encontrado.");
+                    }
+                    keyPress();
                     break;
+                    
                 case 5:
-                    System.out.println("Deletar Produto\n\n");
+                		System.out.println("Deletar Produto\n\n");
+                		System.out.println("\nDigite o ID do Produto: ");
+                    numero = leia.nextInt();
+                    produtos.deletarProduto(numero);
                     
+                    keyPress();
                     break;
+
                 default:
                     System.out.println("\nOpção Inválida!\n");
+                    keyPress();
                     break;
             }
         }
-    }
+    }	  
+          
+    private static void keyPress() {
+		try {
+
+			System.out.println("\nPressione Enter para Continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+
+		}
+	}
 }
